3c807596bc825e2193e9da2e0d5ff25403765162
Dipak Chavda <dipakchavda2374@gmail.com>
2026-07-07 18:10:37 +0530

    token

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,6 +1,9 @@
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (userId) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error('JWT_SECRET is not configured');
+  }
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE || '7d',
   });
